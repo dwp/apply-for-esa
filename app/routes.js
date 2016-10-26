3d9f3cf8e6c57fd7d5fd6ddf7b2d4e23e793c57d
charlesrt <charlesrt.digitaldwp@gmail.com>
2016-10-26 15:19:45 +0100

    Sort out addresses

--- app/routes.js
+++ app/routes.js
@@ -20,7 +20,7 @@ router.post(/address-postcode$/, function (req, res) {
     json: true
   })
   .then(function (body) {
-    
+
     if(body.error === null) {
       var url = req.url.substring(1, req.url.lastIndexOf('/')) + '/address-select';
       res.render(url, { results: body.results }, function(err, html) { res.send(html); });
@@ -29,17 +29,31 @@ router.post(/address-postcode$/, function (req, res) {
     }
   })
   .catch(function (error) {
-    
-    var msg = (error.error) ? error.error.error.message : "Sorry, we couldn't find that postcode"; 
+
+    var msg = (error.error) ? error.error.error.message : "Sorry, we couldn't find that postcode";
     res.render(req.url.substring(1), {addressError: msg, postcode: req.body.postcode}, function(err, html) { res.send(html); });
   });
 
 });
 
 router.post(/address-confirm$/, function (req, res) {
-  
-  var a = req.body.number.split(','); 
+
+  var a = req.body.number.split(',');
   res.render(req.url.substring(1), { address1: a[0], address2: a[1], address3: a[2], postcode: a[3] }, function(err, html) { res.send(html); });
 });
+
+// micro service routes
+router.get('/micro-service/look-up-an-address/confirm-address', function (req, res) {
+
+   var number = req.query.number;
+   console.log(number);
+
+   if (number == "false"){
+     res.redirect("/micro-service/look-up-an-address/manual-address");
+   } else {
+     res.render('micro-service/look-up-an-address/confirm-address', { 'number' : number });
+   }
+
+ });
 
 module.exports = router;
